8ba1924e800a9a383ce9700f26d7c99be489ba98
Waldemar Tomme <waldetomme@gmail.com>
2023-04-12 22:56:37 +0200

    Add DarkLink.Delegation

---
+++ DarkLink.Delegation/IPropertyDelegate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+namespace DarkLink.Delegation;
+
+public interface IPropertyDelegate<T>
+{
+    T Get(object? thisRef, PropertyInfo property);
+
+    void Set(object? thisRef, PropertyInfo property, T value);
+}
---
+++ DarkLink.Delegation/LazyDelegate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace DarkLink.Delegation;
+
+public class LazyDelegate<T> : IPropertyDelegate<T>
+{
+    private readonly Lazy<T> lazy;
+
+    public LazyDelegate(Func<T> getValue)
+    {
+        lazy = new Lazy<T>(getValue);
+    }
+
+    public T Get(object? thisRef, PropertyInfo property) => lazy.Value;
+
+    public void Set(object? thisRef, PropertyInfo property, T value) { }
+}
