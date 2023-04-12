6c3880b74a60b523f84f0bc262ab3f14ce1c98c2
Waldemar Tomme <waldemar.tomme@bluehands.de>
2023-04-12 06:34:07 +0200

    Add DelegateTo attribute

---
+++ DarkLink.Roslyn.Delegation/DelegateTo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using DarkLink.RoslynHelpers;
+using Microsoft.CodeAnalysis;
+
+namespace DarkLink.Roslyn.Delegation;
+
+[GenerateAttribute(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public partial record DelegateTo(INamedTypeSymbol InterfaceType);
--- DarkLink.Roslyn.Delegation/Generator.cs
+++ DarkLink.Roslyn.Delegation/Generator.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using Microsoft.CodeAnalysis;
 
-namespace DarkLink.Roslyn.Delegation
+namespace DarkLink.Roslyn.Delegation;
+
+[Generator]
+public class Generator : IIncrementalGenerator
 {
-    [Generator]
-    public class Generator : IIncrementalGenerator
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        public void Initialize(IncrementalGeneratorInitializationContext context)
-        {
-            context.RegisterPostInitializationOutput(PostInitialize);
+        context.RegisterPostInitializationOutput(PostInitialize);
+    }
 
-            // Initialize
-        }
-
-        private void PostInitialize(IncrementalGeneratorPostInitializationContext context)
-        {
-            // Generate immutable code
-        }
+    private void PostInitialize(IncrementalGeneratorPostInitializationContext context)
+    {
+        DelegateTo.AddTo(context);
     }
 }
